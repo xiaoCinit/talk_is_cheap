3e3b8958ca048b6ebf3ea1df9c88105b4430ea61
Ch@01Ei <xiaolcccccc@gmail.com>
2016-11-08 14:17:52 -0500

    Update 0001.py
    add commits

--- 0001/0001.py
+++ 0001/0001.py
@@ -10,7 +10,7 @@ import uuid
 def createKey(number,length):
     Key = []
     for i in range(number):
-        keyID = str(uuid.uuid4()).replace("-","").upper()[:length]
+        keyID = str(uuid.uuid4()).replace("-","").upper()[:length] #uuid4是uuid生成验证码中的一种方法，uuid4会有重复现象
         if not keyID in Key:
             Key.append(keyID)
 
@@ -19,4 +19,11 @@ if __name__ == "__main__":
     Key_list = createKey(200,8)
     for key in Key_list:
         print key
+        
 #    print createKey(200)
+# uuid1()——基于时间戳。由MAC地址、当前时间戳、随机数生成。可以保证全球范围内的唯一性，但MAC的使用同时带来安全性问题，局域网中可以使用IP来代替MAC
+# uuid2()——基于分布式计算环境DCE（Python中没有这个函数）。算法与uuid1相同，不同的是把时间戳的前4位置换为POSIX的UID。实际中很少用到该方法
+# uuid3()——基于名字的MD5散列值。通过计算名字和命名空间的MD5散列值得到，保证了同一命名空间中不同名字的唯一性，和不同命名空间的唯一性，但同一命名空间的同一名字生成相同的uuid
+# uuid4()——基于随机数。由伪随机数得到，有一定的重复概率，该概率可以计算出来
+# uuid5()——基于名字的SHA-1散列值。算法与uuid3相同，不同的是使用 Secure Hash Algorithm 1 算法
+# 使用uuid生成的字符串使用"-"来 链接，所以需要用replace将"-"替换成""
